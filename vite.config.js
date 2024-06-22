4782222914213bd5b666978d0f7c792276c43143
semanticdata <contact@miguelpimentel.do>
2024-06-21 19:04:51 -0500

    hot-reload when saving inside notes folder

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import ViteRestart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    ViteRestart({
+      restart: ["notes/**"],
+    }),
+    react(),
+  ],
   build: {
     outDir: "_site",
   },
-  base: "./",
-});
+  base: "./"
+})
